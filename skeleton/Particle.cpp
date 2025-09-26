c3deed84a5fa3ee184cfef974c0574b1ee6d10dc
AlvaPina <alvapina@ucm.es>
2025-09-26 10:56:14 +0200

    Particula en proceso

--- skeleton/Particle.cpp
+++ skeleton/Particle.cpp
@@ -1,1 +1,20 @@
 #include "Particle.h"
+
+void Particle::integrate(double t)
+{
+    // Aceleración (ejemplo: gravedad)
+    Vector3D gravity(0.0, -9.81, 0.0);
+
+    // Actualiza la velocidad (Euler semi-implícito)
+    vel = vel + gravity.scalarMul(t);
+
+    // Actualiza la posición usando la nueva velocidad
+    Vector3D pos = Vector3D(pose.p.x, pose.p.y, pose.p.z);
+    pos = pos + vel.scalarMul(t);
+
+    // Actualiza la pose de PhysX
+    pose = physx::PxTransform(physx::PxVec3(pos.getX(), pos.getY(), pos.getZ()));
+
+    // Render Item
+    
+}
--- skeleton/Particle.h
+++ skeleton/Particle.h
@@ -1,11 +1,19 @@
 #pragma once
 
 #include "Vector3D.h"
+#include <PxPhysicsAPI.h>
+#include "RenderUtils.hpp"
 
 class Particle
 {
 public:
 	Particle(Vector3D Pos, Vector3D Vel);
 	~Particle();
+
+	void integrate(double t);
+private:
+	Vector3D vel;
+	physx::PxTransform pose;
+	RenderItem* renderItem;
 };
 
